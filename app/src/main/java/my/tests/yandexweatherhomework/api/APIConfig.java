fd5f75b45963e1103f483ec2c7c343cb9960b113
Alex Mak <arfa3.3@mail.ru>
2022-06-03 22:06:58 +0500

    Initial commit

---
+++ app/src/main/java/my/tests/yandexweatherhomework/api/APIConfig.java
@@ -0,0 +1,8 @@
+package my.tests.yandexweatherhomework.api;
+
+public class APIConfig {
+    public static final String HOST_URL = "https://api.weather.yandex.ru/";
+    public static final double CITY_EKB_LAT = 56.8519000;
+    public static final double CITY_EKB_LON = 60.6122000;
+    public static final String API_YANDEX_KEY = "5cb05502-8a21-42b0-84db-91bc578af92e";
+}
